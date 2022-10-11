924c255c5854822dd096a5a00ec127c3b7407d23
student <student@info-08.cshl.edu>
2022-10-11 16:57:27 -0700

    refined version of posorneg and gccontent trying

--- problemsets/Python2/posorneg.py
+++ problemsets/Python2/posorneg.py
@@ -8,32 +8,16 @@ import math
 input = sys.argv[1]
 number = float(input)
 
-#determining if the number has a decimal or not (decimal being false indicates that it's present)
-if 0 in math.modf(number):
-    decimal = True
-else:
-    decimal = False
-
-#dealing with the fractions
-if number >= 1 or number <= -1:
-    number = int(number)
-elif number == 0:
-    number = int(number)
-elif number > 0:
-    number = 3
-else:
-    number = -3
-
 #sorting the numbers into different categories
-if number > 0:
+if number > 0.0:
     print('positive')
-    if number < 50:
-        if number%2 == 0 and decimal:
+    if number < 50.0:
+        if number%2.0 == 0.0:
             print('it is an even number that is smaller than 50')
-    if number > 50:
-        if number%3 == 0 and decimal:
+    if number > 50.0:
+        if number%3.0 == 0.0:
             print('it is larger than 50 and divisible by 3')
-elif number == 0:
+elif number == 0.0:
     print('zero')
 else:
     print('negative')
