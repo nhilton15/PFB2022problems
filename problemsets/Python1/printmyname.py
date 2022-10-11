b68ac8dd524fc0d90198fd3bbc3cf4ecec0aa45c
student <student@info-08.cshl.edu>
2022-10-11 07:46:23 -0700

    updating printmyname.py to take info from command line

--- problemsets/Python1/printmyname.py
+++ problemsets/Python1/printmyname.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+import sys
 
-#this sets and prints my name
-myname = 'Nick'
-print('My name: ' + myname)
+#this grabs variables from command line
+myname = sys.argv[1]
+favcolor = sys.argv[2]
+favactivity = sys.argv[3]
+favanimal = sys.argv[4]
+
+#this prints the variables (together)
+print('My name: '+myname+'\n'+'My favorite color: '+favcolor+'\n'+'My favorite activity: '+favactivity+'\n'+'My favorite animal: '+favanimal)
+
+#this prints the variables (separately)
+#print('My name: ' + myname)
+#print('My favorite color: ' + favcolor)
+#print('My favorite activity:' , favactivity)
+#print('My favorite animal:' , favanimal)
