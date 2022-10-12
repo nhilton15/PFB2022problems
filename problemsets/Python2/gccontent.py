5adb97763853bdacefbe111a5ca5930f1b80e1a0
student <student@info-08.cshl.edu>
2022-10-11 17:46:07 -0700

    updating gccontent.py

--- problemsets/Python2/gccontent.py
+++ problemsets/Python2/gccontent.py
@@ -5,9 +5,24 @@ import sys
 
 #input of sequences
 sequencelist = sys.argv[1]
+seqnum = sequencelist.count('Rosalind_')
+altseq = sequencelist
 
 #create a dictionary from list of sequences
 seqdict = {}
-seqnum = sequencelist.count('Rosalind_')
-for i in (0,seqnum):
-    
+
+for i in range(0,seqnum):
+    positionS = sequencelist.find('Rosalind_')
+    positionE = -1
+    for j in range(0, 2)
+        positionE = altseq.find('Rosalind_', positionE + 1)
+    seqdict[sequencelist[positionS:positionS+12].append(positionS+13:positionE-2)
+    altseq.replace('Rosalind_','Done',1)
+
+#evaluate GC content in dictionary
+
+
+#determine highest content
+
+
+#display highest sequence name and content
