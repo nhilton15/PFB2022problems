9dedc0ea77b2dd9140cc8704699bccb5c727c134
student <student@info-08.cshl.edu>
2022-10-12 16:01:39 -0700

    forgot to add list comp to print0to99, resolved

--- problemsets/Python4/print0to99.py
+++ problemsets/Python4/print0to99.py
@@ -4,7 +4,8 @@ import sys
 startN = int(sys.argv[1])
 endN = int(sys.argv[2])
 
-for num in range(endN-startN+1):
-    if (startN+num) %2 != 0:
-        print(startN+num)
+#for num in range(endN-startN+1):
+#    if (startN+num) %2 != 0:
+#        print(startN+num)
 
+counter = [print(startN+num) for num in range(endN-startN+1) if (startN+num)%2!=0 ]
