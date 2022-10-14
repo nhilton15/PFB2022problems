433bb4eeadb9d26c4a298be76e676264ee1e49c9
student <student@info-08.cshl.edu>
2022-10-13 18:47:56 -0700

    enzymedict

--- problemsets/Python7/enzymedict.py
+++ problemsets/Python7/enzymedict.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import sys
 
 skipper = 1
 enzdict = {}
@@ -16,4 +17,17 @@ with open('bionet.txt') as bioread:
         else:
             skipper +=1
 
-print(enzdict)
+#print(enzdict)
+
+enzname = sys.argv[1]
+seqtocut = sys.argv[2]
+
+if enzname in enzdict:
+    seqclean = enzdict[enzname].replace('^','')
+    if re.match(seqclean,seqtocut):
+        seqout = re.sub(seqclean,enzdict[enzname],seqtocut)
+        print(seqout)
+        seqsplit = seqout.split('^')
+        print(seqsplit)
+        seqsplit = seqsplit.sort(key=len)
+        print(seqsplit)
