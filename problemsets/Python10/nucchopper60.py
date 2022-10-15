40b4f5c4df40f61b76cf0e55fb0010c7141f9bf2
student <student@info-08.cshl.edu>
2022-10-15 11:33:13 -0700

    first half of python 10

--- problemsets/Python10/nucchopper60.py
+++ problemsets/Python10/nucchopper60.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 
-dnaseq = input('Input a sequence of dna: ')
-
-def stringcut60(dna):
+def stringcut60(dna, length):
     cutdna = ''
     newcut = 0
+    dna = dna.replace('\n','')
     for nuc in range(len(dna)):
-        if (nuc)%60 == 0 and nuc != 0:
+        if (nuc)%int(length) == 0 and nuc != 0:
             oldcut = newcut
             newcut = nuc
             cutdna = cutdna +'\n'+dna[oldcut:newcut]
     cutdna = cutdna +'\n'+dna[newcut:]
+    cutdna = cutdna[1:]
     return cutdna
 
-print(stringcut60(dnaseq))
+dnaseq = input('Enter a fasta file name: ')
+dnalen = input('Enter a length: ')
+
+with open(dnaseq,'r') as fastaread:
+    chunk = ''
+    for line in fastaread:
+        if line.find('>') != -1:
+            print(stringcut60(chunk, dnalen), end='')
+            chunk = ''
+            print(line, end='')
+        else:
+            chunk = chunk + line
+    print(stringcut60(chunk, dnalen))
 
